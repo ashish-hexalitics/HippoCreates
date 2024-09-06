8e15e69a7dcad4a14a0e47a1e08225f27c1ca4e6
ashish <ashish.vishwakarma@hexalitics.com>
2024-09-06 15:58:57 +0530

    upaded url

--- src/store/baseQuery.ts
+++ src/store/baseQuery.ts
@@ -10,7 +10,7 @@ const baseQueryWithInterceptor: BaseQueryFn<
 > = async (args, api, extraOptions) => {
   const result = await fetchBaseQuery({
     // baseUrl: "http://localhost:3000/api",
-    baseUrl: "https://ec2-16-170-243-143.eu-north-1.compute.amazonaws.com:3000/api",
+    baseUrl: "http://ec2-16-170-243-143.eu-north-1.compute.amazonaws.com:3000/api",
   })(args, api, extraOptions);
 
   // Check for 403 status
